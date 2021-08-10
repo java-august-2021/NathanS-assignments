a2b088d77a087ae1cfe7b8b29bbef194235fa51a
Nathan Seals <nathanseals@Nathans-MacBook.local>
2021-08-10 13:14:54 -0500

    mapping practice

---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/MapOfTheHashmatique.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Set;
+public class MapOfTheHashmatique {
+    public static void main(String[] args) {
+        HashMap<String, String> trackList = new HashMap<String, String>();
+        trackList.put("Rain On Me", "I didn't ask for a free ride, I only asked you to show me a real good time");
+        trackList.put("Express Yourself", "Don't go for second best, put your love to the test");
+        trackList.put("Smells Like Teen Spirit", "With the lights out, this is dangerous");
+        trackList.put("Radioactive", "When you're around me I'm radioactive");
+        System.out.println(trackList.get("Smells Like Teen Spirit")); // pull out one song by title
+
+        Set<String> tracks = trackList.keySet(); //pull out all songs using format "Track: Lyric"
+        for(String key : tracks) {
+            System.out.println(key + ": " + trackList.get(key));
+        } //for loop saying "print all the song lyrics by Track
+    }
+
+}
